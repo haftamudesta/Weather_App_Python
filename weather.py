17eb20dae83e941036051ef62096578dc3c4b4f8
haftamudesta <desta451616@hotmail.com>
2025-07-12 13:35:52 -0800

    Add back end and front end

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 
 def get_current_weather(city="Addis Ababa"):
-    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid= {os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
     weather_data = requests.get(request_url).json()
     return weather_data
 
@@ -15,6 +15,8 @@ def get_current_weather(city="Addis Ababa"):
 if __name__ == "__main__":
     print("\n***Get current weather condition...***\n")
     city = input("\nPlease enter city name: ")
+    if not city or not city.strip():
+        city = "Addis Ababa"
     weather_data = get_current_weather(city)
     print("\n")
     pprint(weather_data)
